e7990dfa3f660d4c68c9b9ea670987eb2a60430d
https://hll2975.github.io/M-Hoja-de-Vida-/Hernán-Luis-IT <56049876+hernan2975@users.noreply.github.com>
2025-07-10 16:44:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-
 import os
 from flask import Flask
 from app.routes import app_routes
 
-app = Flask(__name__)
+app = Flask(__name__)  # ⬅️ ¡Esta línea debe estar afuera!
+
 app.register_blueprint(app_routes)
 
+# Este bloque es solo para correr con flask directamente (opcional)
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
