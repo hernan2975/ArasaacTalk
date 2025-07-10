cdf675cc875e84bd369405ea8637f68c4aac2738
https://hll2975.github.io/M-Hoja-de-Vida-/Hernán-Luis-IT <56049876+hernan2975@users.noreply.github.com>
2025-07-10 12:49:22 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,20 +1,3 @@
 #!/bin/bash
-
-APP_DIR="/opt/arasaac-offline"
-
-cd "$APP_DIR" || exit 1
-
-# Crear entorno virtual si no existe
-if [ ! -d "venv" ]; then
-  python3 -m venv venv
-fi
-
-# Activar entorno virtual
-source venv/bin/activate
-
-# Instalar dependencias si es necesario
-pip install --upgrade pip
-pip install -r requirements.txt
-
-# Lanzar la app Flask
-python3 app.py
+export FLASK_APP=app.py
+flask run --host=0.0.0.0 --port=5000
