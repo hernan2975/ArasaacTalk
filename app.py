6e1050bb85290dbee19c5f9ad936d69749190ef3
https://hll2975.github.io/M-Hoja-de-Vida-/Hernán-Luis-IT <56049876+hernan2975@users.noreply.github.com>
2025-07-10 16:42:19 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+
+import os
+from flask import Flask
+from app.routes import app_routes
+
+app = Flask(__name__)
+app.register_blueprint(app_routes)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
