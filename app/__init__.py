29fbd1c66a7a1a60cb4ee7da1189d3c165607d43
https://hll2975.github.io/M-Hoja-de-Vida-/Hernán-Luis-IT <56049876+hernan2975@users.noreply.github.com>
2025-07-10 17:40:58 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,7 @@
-# Paquete inicializador vacío
+from flask import Flask
+from app.routes import app_routes
+
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(app_routes)
+    return app
