b2538b98ef58fe1e916a29e9ee7902ee038d74c4
https://hll2975.github.io/M-Hoja-de-Vida-/Hernán-Luis-IT <56049876+hernan2975@users.noreply.github.com>
2025-07-10 12:50:21 -0300

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,32 +1,7 @@
-from flask import Blueprint, render_template, send_file, abort
-import os
-import re
+from flask import Blueprint, render_template
 
-main = Blueprint('main', __name__)
-BASE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+app_routes = Blueprint('app_routes', __name__)
 
-@main.route("/")
-def home():
+@app_routes.route("/")
+def index():
     return render_template("index.html")
-
-@main.route("/preload/pictos/datos/<picto_id>.json")
-def serve_picto_data(picto_id):
-    if not re.fullmatch(r"\d+", picto_id):
-        abort(400, description="ID inválido")
-    
-    path = os.path.join(BASE_PATH, "preload", "pictos", "datos", f"{picto_id}.json")
-    if not os.path.exists(path):
-        abort(404, description="Archivo no encontrado")
-    
-    return send_file(path, mimetype="application/json")
-
-@main.route("/preload/pictos/imagenes/<picto_id>.png")
-def serve_picto_image(picto_id):
-    if not re.fullmatch(r"\d+", picto_id):
-        abort(400, description="ID inválido")
-
-    path = os.path.join(BASE_PATH, "preload", "pictos", "imagenes", f"{picto_id}.png")
-    if not os.path.exists(path):
-        abort(404, description="Imagen no encontrada")
-
-    return send_file(path, mimetype="image/png")
