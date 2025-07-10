c02401bd41f3e043edc32f0116f43314a6ec8a41
https://hll2975.github.io/M-Hoja-de-Vida-/Hernán-Luis-IT <56049876+hernan2975@users.noreply.github.com>
2025-07-10 12:48:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask
-from app.routes import main
+from app.routes import app_routes
 
 app = Flask(__name__)
-app.register_blueprint(main)
+app.register_blueprint(app_routes)
 
 if __name__ == "__main__":
     app.run(debug=True)
