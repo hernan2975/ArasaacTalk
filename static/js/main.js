4e26781aedbb621c9deb157ad040a07889f17a4b
https://hll2975.github.io/M-Hoja-de-Vida-/Hernán-Luis-IT <56049876+hernan2975@users.noreply.github.com>
2025-07-10 18:59:19 -0300

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,10 @@
 import { cargarCategorias } from "./preload.js";
 import { reproducirFrase } from "./speech.js";
-import { construirBotonesCategoria, limpiarFrase } from "./ui.js";
+import { construirBotonesCategoria, limpiarFrase, construirFavoritos } from "./ui.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
   await cargarCategorias(construirBotonesCategoria);
+  await construirFavoritos();
 
   document.getElementById("play-btn").onclick = reproducirFrase;
   document.getElementById("clear-btn").onclick = limpiarFrase;
