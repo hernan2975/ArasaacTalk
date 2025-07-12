d3eba349fcb6d0aaf7bb3170071ff969a5cc0534
https://hll2975.github.io/M-Hoja-de-Vida-/Hernán-Luis-IT <56049876+hernan2975@users.noreply.github.com>
2025-07-12 08:12:35 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,15 @@
+import os
 from flask import Flask
 from app.routes import app_routes
 
 def create_app():
-    app = Flask(__name__)
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    templates_dir = os.path.join(base_dir, "..", "templates")
+
+    app = Flask(__name__, template_folder=templates_dir)
 
     app.config["JSON_SORT_KEYS"] = False
-    app.config["MAX_CONTENT_LENGTH"] = 2 * 1024 * 1024  # 2MB por request
+    app.config["MAX_CONTENT_LENGTH"] = 2 * 1024 * 1024
 
     app.register_blueprint(app_routes)
-
     return app
